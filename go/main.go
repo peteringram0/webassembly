7d5129426749505be2e7c99fcd11db5f996cf129
Peter Ingram <p.ingram@managedracks.co.uk>
2019-02-14 16:32:17 +0000

    missing commit

--- go/main.go
+++ go/main.go
@@ -7,6 +7,7 @@ import (
 
 func registerCallbacks() {
 	js.Global().Set("add", js.NewCallback(add))
+	js.Global().Set("fib", js.NewCallback(FibonacciRecursion))
 }
 
 func main() {
@@ -23,4 +24,12 @@ func add(i []js.Value) {
 	js.Global().Set("output", js.ValueOf(i[0].Int()+i[1].Int()))
 	data := js.ValueOf(i[0].Int() + i[1].Int()).String()
 	println(data)
+}
+
+func FibonacciRecursion(n int) {
+	if n <= 1 {
+		data := js.ValueOf(n.Int())
+		println(data)
+	}
+	return FibonacciRecursion(n-1) + FibonacciRecursion(n-2)
 }
--- ts/main.ts
+++ ts/main.ts
@@ -5,3 +5,8 @@
 export function add(x: i32, y: i32): i32 {
   return x + y;
 }
+
+export function fib(num: i32): i32 {
+  if (num <= 1) return 1;
+  return fib(num - 1) + fib(num - 2);
+}
